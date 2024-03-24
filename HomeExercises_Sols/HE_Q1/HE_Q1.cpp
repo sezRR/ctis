42b0003b19afade3e01b68bfba800c6b48531dcc
szRR <sezer.tetik2005@gmail.com>
2024-03-24 16:00:45 +0300

    ☑️ HomeExercises_Sols and HE_Q1 added.

---
+++ HomeExercises_Sols/HE_Q1/HE_Q1.cpp
@@ -0,0 +1,45 @@
+/*
+*	WRITE A PROGRAM TO DISPLAY THE FOLLOWING TRIANGLE, USING A FUNCTION
+*
+*	OUTPUT:
+*	    *
+*	   ***
+*	  *****
+*	 *******
+*	**********
+* 
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#define ROW_COUNT 5
+#define OUTPUT_CHARACTER '*'
+
+#include <stdio.h>
+
+void drawChar(int times, char character)
+{
+	for (int i = 0; i < times; i++)
+	{
+		printf("%c", character);
+	}
+
+	if (character != ' ')
+		printf("\n");
+}
+
+void drawTriangle(int rowCount, char character)
+{
+	for (int i = 1; i <= rowCount; i++)
+	{
+		drawChar(rowCount - i, ' ');
+		drawChar(i * 2 - 1, character);
+	}
+}
+
+int main(void)
+{
+	drawTriangle(ROW_COUNT, OUTPUT_CHARACTER);
+
+	return 0;
+}
